c89e0a74da422248d2ea6e8f9b127770f36d751c
Mohamed <dtmohamed95@gmail.com>
2018-09-20 18:01:20 +0100

    added fab + onscroll listener

--- app/src/main/java/test/mo/timer/MainActivity.java
+++ app/src/main/java/test/mo/timer/MainActivity.java
@@ -1,13 +1,39 @@
 package test.mo.timer;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.recyclerView);
+        fab =findViewById(R.id.fab_Add);
+
+
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                if(dy < 0){
+                    fab.show();
+                } else if (dy > 0){
+                    fab.hide();
+                }
+            }
+        });
     }
 }
