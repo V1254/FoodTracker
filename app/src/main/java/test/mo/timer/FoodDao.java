ddd72dd9958ce05e9c8f3a6768dacfe2d806236b
Mohamed <dtmohamed95@gmail.com>
2018-09-20 20:50:31 +0100

    updated dao and database

---
+++ app/src/main/java/test/mo/timer/FoodDao.java
@@ -0,0 +1,4 @@
+package test.mo.timer;
+
+public interface FoodDao {
+}
---
+++ app/src/main/java/test/mo/timer/foodDatabase.java
@@ -0,0 +1,6 @@
+package test.mo.timer;
+
+import android.arch.persistence.room.RoomDatabase;
+
+public abstract class foodDatabase extends RoomDatabase {
+}
