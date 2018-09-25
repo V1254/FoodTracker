97b962ab2d88621697009195294cfd45565d47fa
Mohamed <dtmohamed95@gmail.com>
2018-09-25 09:52:24 +0100

    refractoring

--- app/src/main/java/test/mo/FoodTracker/Food.java
+++ app/src/main/java/test/mo/FoodTracker/Food.java
@@ -2,6 +2,7 @@ package test.mo.FoodTracker;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
@@ -18,16 +19,25 @@ public class Food {
     private String foodName;
 
     @ColumnInfo(name = "date_added")
-    private String startDate;
+    private Long startDate;
 
     @ColumnInfo(name = "expiry_date")
-    private String expiryDate;
+    private Long expiryDate;
 
-    public Food(String foodName, String startDate, String expiryDate) {
+    public Food(String foodName, Long startDate, Long expiryDate) {
         this.foodName = foodName;
         this.startDate = startDate;
         this.expiryDate = expiryDate;
     }
+
+    @Ignore
+    public Food(int id, String name, Long startDate, Long expiryDate){
+        this.id = id;
+        this.foodName = name;
+        this.startDate = startDate;
+        this.expiryDate = expiryDate;
+    }
+
 
     public int getId() {
         return id;
@@ -45,19 +55,19 @@ public class Food {
         this.foodName = foodName;
     }
 
-    public String getStartDate() {
+    public Long getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    public void setStartDate(Long startDate) {
         this.startDate = startDate;
     }
 
-    public String getExpiryDate() {
+    public Long getExpiryDate() {
         return expiryDate;
     }
 
-    public void setExpiryDate(String expiryDate) {
+    public void setExpiryDate(Long expiryDate) {
         this.expiryDate = expiryDate;
     }
 }
