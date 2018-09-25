85074c6a171eeb6d9411df8de1fce71a38a45d8e
Mohamed <dtmohamed95@gmail.com>
2018-09-25 16:34:10 +0100

    moved to package

--- app/src/main/java/test/mo/FoodTracker/View/MainActivity.java
+++ app/src/main/java/test/mo/FoodTracker/View/MainActivity.java
@@ -1,8 +1,7 @@
-package test.mo.FoodTracker;
+package test.mo.FoodTracker.View;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -10,17 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.View;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import test.mo.FoodTracker.Adapter;
+import test.mo.FoodTracker.DateConverter;
 import test.mo.FoodTracker.Model.Food;
-import test.mo.FoodTracker.Model.FoodDatabase;
-import test.mo.FoodTracker.View.AddActivity;
+import test.mo.FoodTracker.R;
 import test.mo.FoodTracker.ViewModel.FoodListViewModel;
 
 public class MainActivity extends AppCompatActivity {
